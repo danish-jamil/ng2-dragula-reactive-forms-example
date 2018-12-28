0ecf45ec364a8b5e257885ab1af0f153fe486dc2
Danish Jamil <mrdanishjameel@gmail.com>
2018-12-28 19:55:42 +0500

    ng2-dragula example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DragulaService } from 'ng2-dragula';
+import { FormArray, FormGroup, FormControl } from '@angular/forms';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-drop';
+  posts = new FormArray([]);
+  constructor(
+    private http: HttpClient,
+    private dragulaService: DragulaService
+  ) {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((posts: Post[]) =>
+        posts.splice(0, 20).forEach(post =>
+          this.posts.push(
+            new FormGroup({
+              id: new FormControl(post.id),
+              title: new FormControl(post.title)
+            })
+          )
+        )
+      );
+    this.dragulaService.drop.subscribe(value => {
+      const [
+        el,
+        container,
+        container2,
+        nextElement
+      ]: HTMLElement[] = value.splice(1);
+      console.log(el, nextElement.attributes.getNamedItem('id'));
+      console.log(this.posts.controls);
+      this.posts.controls.forEach((control, index) =>
+        control.get('id').setValue(index + 1)
+      );
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DragulaModule } from 'ng2-dragula';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DragulaModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
